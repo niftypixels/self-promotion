1b98695e7778eaec3808a3120947c0f83ffb1f98
Scott Twede <scott@niftypixels.com>
2025-03-31 12:35:34 -0700

    chore: adds debounce hook

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,23 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+export default function useDebounce(callback, delay) {
+  const timeoutRef = useRef();
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  return useCallback((...args) => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  }, [callback, delay]);
+};
