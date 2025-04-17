41146f7daed59d4d1d6eaf671231d03b856cc00b
Scott Twede <scott@niftypixels.com>
2025-04-17 00:44:37 -0700

    feat: update footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,18 @@ function Footer() {
     <footer className='container'>
       <img src='/sadmac.png' alt='' />
       <p>Made with <strong>❤</strong> in OC</p>
-      <div>
-        <a href='//github.com/niftypixels/self-promotion/' target='_blank'>GitHub</a>
-        <a href='//linkedin.com/in/niftypixels/' target='_blank'>LinkedIn</a>
-        <a href='/Scott-Twede-Resume.pdf' target='_blank'>PDF Resume</a>
-      </div>
       <span>SCOTT <br /> TWEDE <br /> &copy;{new Date().getFullYear()}</span>
+      <div>
+        <a href='//github.com/niftypixels/self-promotion/' target='_blank'>
+          GitHub
+        </a>
+        <a href='//linkedin.com/in/niftypixels/' target='_blank'>
+          LinkedIn
+        </a>
+        <a href='/Scott-Twede-Resume.pdf' target='_blank'>
+          PDF
+        </a>
+      </div>
     </footer>
   )
 }
