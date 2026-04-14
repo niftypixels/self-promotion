38fe495dfffd8f5165e246fe822555271e1df32d
Scott Twede <scott@niftypixels.com>
2026-04-13 17:28:54 -0700

    chore: adjust experience layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
       <p>Made with <img src='heart.svg' alt='❤' /> in OC</p>
       <span>SCOTT <br /> TWEDE <br /> &copy;{new Date().getFullYear()}</span>
       <a href='mailto:scott@twede.dev'>
-        Contact
+        Email
       </a>
       <a href='//github.com/niftypixels/self-promotion/' target='_blank'>
         GitHub
