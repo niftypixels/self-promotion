ec65d89417780ae6a5ec54c13377727b2be087a7
Scott Twede <scott@niftypixels.com>
2026-05-13 17:29:03 -0700

    chore: adds Magic8Ball stubs

--- src/components/Magic8Ball.jsx
+++ src/components/Magic8Ball.jsx
@@ -3,12 +3,14 @@ import '../styles/Magic8Ball.scss';
 function Magic8Ball({ focus = face1 }) {
   return (
     <div id='magic8ball'>
-      <div className='face1'>sadmac</div>
-      <div className='face2'>email</div>
-      <div className='face3'>github</div>
-      <div className='face4'>linkedin</div>
-      <div className='face5'>pdf</div>
-      <div className='face6'>happymac</div>
+      <div id='cube'>
+        <div className='face1'>sadmac</div>
+        <div className='face2'>email</div>
+        <div className='face3'>github</div>
+        <div className='face4'>linkedin</div>
+        <div className='face5'>pdf</div>
+        <div className='face6'>happymac</div>
+      </div>
     </div>
   )
 }
