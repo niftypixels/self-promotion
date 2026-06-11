04e5cea8b58fa29115138e9002247ebd7f84f5a4
Scott Twede <scott@niftypixels.com>
2026-06-11 14:58:30 -0700

    chore: oxford comma

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import '../styles/About.scss';
 
-export const ABOUT = 'I have 20+ years of expertise building creative interactive applications for global brands including PlayStation, Samsung, ESPN, Disney, Paramount, Lionsgate, HBO and UFC.\n\nCurrently consulting; open to contract and full-time opportunities.';
+export const ABOUT = 'I have 20+ years of expertise building creative interactive applications for global brands including PlayStation, Samsung, ESPN, Disney, Paramount, Lionsgate, HBO, and UFC.\n\nCurrently consulting; open to contract and full-time opportunities.';
 
 function About() {
   return (
