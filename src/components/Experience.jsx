97423cf3850ed58d910a2dfed84eb0e7c65f52a3
Scott Twede <scott@niftypixels.com>
2026-07-24 19:38:16 -0700

    chore: wimplified experience template

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,32 +1,59 @@
 import '../styles/Experience.scss';
 
+const JOBS = [
+  {
+    company: 'Sony Interactive Entertainment',
+    startDate: new Date('07/2014'),
+    endDate: new Date('12/2023'),
+    location: 'Aliso Viejo + San Francisco, CA',
+    role: 'Tech Lead, Senior Software Engineer'
+  },
+  {
+    company: 'RED Interactive Agency',
+    startDate: new Date('09/2010'),
+    endDate: new Date('06/2014'),
+    location: 'Santa Monica, CA',
+    role: 'Front-end Engineer'
+  }
+];
+
 const AWARDS = [
   {
     date: new Date('6/22/14'),
     href: '//thefwa.com/cases/el-rey-network',
-    project: "El Rey Network",
+    project: "El Rey Network"
   },
   {
     date: new Date('2/5/13'),
     href: '//thefwa.com/cases/ufc-social',
-    project: "UFC Social",
+    project: "UFC Social"
   },
   {
     date: new Date('2/24/12'),
     href: '//thefwa.com/cases/the-hunt-for-the-golden-pistachio',
-    project: "The Hunt for the Golden Pistachio",
+    project: "The Hunt for the Golden Pistachio"
   },
   {
     date: new Date('12/4/11'),
     href: '//thefwa.com/cases/lucasfilm-s-star-wars-visualizer',
-    project: "Lucasfilm's Star Wars Visualizer",
-  },
+    project: "Lucasfilm's Star Wars Visualizer"
+  }
 ];
 
 function Experience() {
   return (
     <section className='container' id='experience'>
       <div>
+        {JOBS.map(({ company, startDate, endDate, location, role }, index) => (
+          <h3 className='blob' style={{ '--bob-delay': '0s' }} key={index}>
+            <strong>{company}</strong>
+            <span>
+              {startDate.toDateString()} &mdash; {endDate.toDateString()}
+              <br /> &raquo; {location}
+            </span>
+            {role}
+          </h3>
+        ))}
 
         <h3 className='blob' style={{ '--bob-delay': '0s' }}>
           <strong>Sony Interactive Entertainment</strong>
